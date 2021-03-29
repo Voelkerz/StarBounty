f575e05811cf670b299e677d746e9d8b4fa2afe5
Voelkerz <Voelkerz@gmail.com>
2021-03-28 20:36:43 -0400

    have path vector list now

--- Assets/Game/Scripts/Ship/LineMovementController.cs
+++ Assets/Game/Scripts/Ship/LineMovementController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
@@ -7,6 +8,9 @@ public class LineMovementController : MonoBehaviour {
     private GameObject movementLine;
     private GameObject movementCursor;
     private MoreMountains.Tools.MMBezierLineRenderer bezierLine;
+    private MoreMountains.Tools.MMPath path;
+    private LineRenderer lineRenderer;
+    private int size;
 
     // Start is called before the first frame update
     void Start() {
@@ -14,7 +18,7 @@ public class LineMovementController : MonoBehaviour {
         movementLine = (GameObject)PrefabUtility.InstantiatePrefab(movementLinePrefab);
         movementLine.transform.position = new Vector3(transform.position.x + 2, transform.position.y, movementLine.transform.position.z);
 
-        // instantiate the movement cursor
+        // instantiate the movement cursor and start it in front of ship
         movementCursor = (GameObject)PrefabUtility.InstantiatePrefab(movementCursorPrefab);
         movementCursor.transform.position = new Vector3(transform.position.x + 4, transform.position.y, movementCursor.transform.position.z);
 
@@ -25,10 +29,22 @@ public class LineMovementController : MonoBehaviour {
         bezierLine.AdjustmentHandles.SetValue(gameObject.transform, 1);
         bezierLine.AdjustmentHandles.SetValue(movementLine.transform, 2);
         bezierLine.AdjustmentHandles.SetValue(movementCursor.transform, 3);
+
+        // get the built lineRenderer
+        lineRenderer = movementLine.GetComponent<LineRenderer>();
+        size = lineRenderer.positionCount;
+
+        // get pathing editor
+        path = GetComponent<MoreMountains.Tools.MMPath>();
     }
 
     // Update is called once per frame
     void Update() {
-
+        // create AI path for ship to follow based on line points`
+        for (int i = 0; i < size; i++) {
+            MoreMountains.Tools.MMPathMovementElement element = new MoreMountains.Tools.MMPathMovementElement();
+            element.PathElementPosition = lineRenderer.GetPosition(i);
+            path.PathElements[i] = element;
+        }
     }
 }
---
+++ Assets/Game/Scripts/TDE Overrides/Z_MMPath.cs
@@ -0,0 +1,7 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Z_MMPath : MoreMountains.Tools.MMPath {
+    
+}
